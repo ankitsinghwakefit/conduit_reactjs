1082971380e10ef4f29afcdf583015b1a77337f9
ankit <ankitsinghwakefit@gmail.com>
2020-03-17 09:47:33 +0530

    logo changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,14 +9,14 @@ class Header extends React.Component{
              
                 <nav class="navbar" role="navigation" aria-label="main navigation">
                 <div className="navbar-brand">
-                <a className="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                <Link to="/" className="navbar-item" href="https://bulma.io"> InstaPost </Link>
+                
 
     <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" />
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
-    </a>
+    
   </div>
 
   <div id="navbarBasicExample" className="navbar-menu">
